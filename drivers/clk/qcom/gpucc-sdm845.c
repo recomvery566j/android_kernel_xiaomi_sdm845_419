01e1a3ab89167599da476395ba968f7da960e794
recomvery566j <80522197+recomvery566j@users.noreply.github.com>
2026-04-21 19:06:04 +0800

    Update gpucc-sdm845.c

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -275,6 +275,7 @@ static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(596000000, P_CRC_DIV,  1, 0, 0),
 	F(675000000, P_CRC_DIV,  1, 0, 0),
 	F(710000000, P_CRC_DIV,  1, 0, 0),
+    F(810000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
 
