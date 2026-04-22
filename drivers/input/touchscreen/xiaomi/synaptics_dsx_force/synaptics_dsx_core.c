1bb9cd5d9c932842ce07c6a8555b5f6fd131e4a1
recomvery566j <80522197+recomvery566j@users.noreply.github.com>
2026-04-23 03:17:47 +0800

    Update synaptics_dsx_core.c

--- drivers/input/touchscreen/xiaomi/synaptics_dsx_force/synaptics_dsx_core.c
+++ drivers/input/touchscreen/xiaomi/synaptics_dsx_force/synaptics_dsx_core.c
@@ -1846,21 +1846,31 @@ static int synaptics_rmi4_f12_abs_report(struct synaptics_rmi4_data *rmi4_data,
 #endif
 
 		if (rmi4_data->stylus_enable) {
-			stylus_presence = 0;
-			input_report_key(rmi4_data->stylus_dev,
-					BTN_TOUCH, 0);
-			input_report_key(rmi4_data->stylus_dev,
-					BTN_TOOL_PEN, 0);
+			    stylus_presence = 0;
+			    input_report_key(rmi4_data->stylus_dev,
+					            BTN_TOUCH, 0);
+			    input_report_key(rmi4_data->stylus_dev,
+					            BTN_TOOL_PEN, 0);
 			if (rmi4_data->eraser_enable) {
 				input_report_key(rmi4_data->stylus_dev,
-						BTN_TOOL_RUBBER, 0);
+						        BTN_TOOL_RUBBER, 0);
 			}
 		}
 
 		rmi4_data->touchs = 0;
 	}
 
-	input_sync(rmi4_data->input_dev);
+	{
+		static unsigned long last_sync_time_f12 = 0;
+		static unsigned char last_touch_count_f12 = 0;
+		unsigned long current_time_f12 = jiffies;
+
+		if (touch_count != last_touch_count_f12 || time_after(current_time_f12, last_sync_time_f12 + msecs_to_jiffies(12))) {
+			    input_sync(rmi4_data->input_dev);
+			    last_sync_time_f12 = current_time_f12;
+			    last_touch_count_f12 = touch_count;
+		}
+	}
 
 	mutex_unlock(&(rmi4_data->rmi4_report_mutex));
 
